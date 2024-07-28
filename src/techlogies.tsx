1a3df0c98204d78c93410fde9f55d0dc9e5522b7
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2024-07-28 22:53:14 +0000

    Adicionado tecnlogias que utilizo

--- src/techlogies.tsx
+++ src/techlogies.tsx
@@ -1,44 +1,36 @@
 const TechlogiesData = [
-    {
-        "id": 1,
-        "name": "JavaScript"
-    },
-    {
-        "id": 2,
-        "name": "React JS"
-    },
-    {
-        "id": 3,
-        "name": "React Native"
-    },
-    {
-        "id": 4,
-        "name": "TypeScript"
-    },
-    {
-        "id": 5,
-        "name": "NextJs"
-    },
-    {
-        "id": 6,
-        "name": "NodeJs"
-    },
-    {
-        "id": 7,
-        "name": "Firebase"
-    },
-    {
-        "id": 7,
-        "name": "MySQL"
-    },
-    {
-        "id": 8,
-        "name": "Git"
-    },
-    {
-        "id": 9,
-        "name": "GitHub"
-    }
-]
+  {
+    id: 1,
+    name: "JavaScript",
+  },
+  {
+    id: 2,
+    name: "React JS",
+  },
+  {
+    id: 3,
+    name: "C#",
+  },
+  {
+    id: 4,
+    name: "TypeScript",
+  },
+  {
+    id: 5,
+    name: "Git",
+  },
+  {
+    id: 6,
+    name: ".Net Core",
+  },
+  {
+    id: 7,
+    name: "Entity Framework Core",
+  },
+  {
+    id: 8,
+    name: "SQL Server",
+  },
+];
 
 export default TechlogiesData;
