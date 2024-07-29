7ee62eb2af479dba4d7b21c192f429fc89f996e8
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2024-07-29 01:59:58 +0000

    Adiconado componente sidebar

---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #171923;
+  position: fixed;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+  width: 300px;
+  animation: showSidebar 0.4s;
+
+  > svg {
+    position: fixed;
+    color: white;
+    width: 30px;
+    height: 30px;
+    margin-top: 32px;
+    margin-left: 32px;
+    cursor: pointer;
+  }
+
+  @keyframes showSidebar {
+    from {
+      opacity: 0;
+      width: 0;
+    }
+    to {
+      opacity: 1;
+      width: 300px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  margin-top: 100px;
+`;
